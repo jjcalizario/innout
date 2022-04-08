9eb61cea6872d238bfb445081a68f2c1c00fb087
Jean Jacobe <jean.cdatelecom@gmail.com>
2022-04-08 17:20:57 -0300

    updt

--- public/index.php
+++ public/index.php
@@ -1,10 +1,8 @@
 <?php
 require_once (dirname(__FILE__,2). '/src/config/config.php');
+require_once (dirname(__FILE__,2). '/src/views/login.php');
 
-$sql= "SELECT * FROM users";
-$result = Database::getResultFromQuery($sql);
 
-while($row = $result->fetch_assoc()){
-    print_r($row);
-    echo '<br>';
-}
+
+
+
--- src/models/User.php
+++ src/models/User.php
@@ -4,4 +4,15 @@ require_once (realpath(MODEL_PATH . '/Model.php'));
 
 class User extends Model{
 
+    protected static $tableName ="users";
+    protected static $colums = [
+        'id',
+        'name',
+        'password',
+        'email',
+        'start_date',
+        'end_date',
+        'is_admin',
+    ];
+
 }
