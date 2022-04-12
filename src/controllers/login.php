a2f5443d40a18b3811178797386429fedc5aac91
Jean Jacobe <jean.cdatelecom@gmail.com>
2022-04-12 12:29:42 -0300

    updt

--- src/controllers/login.php
+++ src/controllers/login.php
@@ -7,6 +7,7 @@ if(count($_POST) > 0){
     
     try{
         $user = $login->checkLogin();
+        echo "Usuário {$user->name} logado :)";
         header("Location: day_records.php");
 
     }catch(AppException $e){
