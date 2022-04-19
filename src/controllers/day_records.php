eb1d7f40e32d251ba2fbba32820de58d2ee00ef1
Jean Jacobe <jean.cdatelecom@gmail.com>
2022-04-19 17:23:17 -0300

    t

--- src/controllers/day_records.php
+++ src/controllers/day_records.php
@@ -2,5 +2,10 @@
 
 session_start();
 requireValidSession();
-loadTemplateView('day_records');
 
+
+$date = (new DateTime())->getTimestamp();
+setlocale(LC_ALL, 'pt_BR.utf-8');
+$today = date('d M y', strtotime($date));
+
+loadTemplateView('day_records',['today' =>$today]);
